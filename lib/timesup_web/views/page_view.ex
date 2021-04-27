e4cf37d0b6be52fd53ced3237aa32bf6f5d27922
lud <ludovic@demblans.com>
2021-04-27 22:03:09 +0200

    modifs textes

--- lib/timesup_web/views/page_view.ex
+++ lib/timesup_web/views/page_view.ex
@@ -11,10 +11,38 @@ defmodule TimesupWeb.PageView do
 
   def team_summary(game, i) do
     %{
+      index: i,
       number: i + 1,
       players: Game.team(game, i),
       points: Game.team_points(game, i),
       total: Game.team_points(game, i) |> Map.values() |> Enum.sum()
     }
+  end
+
+  @name_1 "Les eFentastic"
+  @name_2 "Les MerveillouseFen"
+
+  def team_name(0) do
+    "Équipe 1 : #{@name_1}"
+  end
+
+  def team_name(1) do
+    "Équipe 2 : #{@name_2}"
+  end
+
+  def team_name(team_index) do
+    "Équipe " <> to_string(team_index + 1)
+  end
+
+  def team_name_alt(0) do
+    "1 : #{@name_1}"
+  end
+
+  def team_name_alt(1) do
+    "2 : #{@name_2}"
+  end
+
+  def team_name_alt(team_index) do
+    "#{team_index + 1}"
   end
 end
